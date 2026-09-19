d98016ffcd8572045cfaf801bbc2ed46c66998cf
Dunkrick <rithwickgurram05@gmail.com>
2026-09-19 18:26:42 +0530

    fix/remove debug logging

--- main.go
+++ main.go
@@ -96,7 +96,6 @@ func (s *Server) createURLHandler(w http.ResponseWriter, r *http.Request) {
 	).Scan(&id)
 
 	if err != nil {
-		fmt.Println("Create URL DB error:", err)
 		http.Error(w, "failed to create URL", http.StatusInternalServerError)
 		return
 	}
